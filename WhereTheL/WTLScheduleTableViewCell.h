70fe899dbff3090f201292cf23d68b4cf83235a9
lukasthoms <luk@stho.ms>
2015-07-12 18:06:23 -0400

    basic UI implemented

---
+++ WhereTheL/WTLScheduleTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WTLScheduleTableViewCell.h
+//  WhereTheL
+//
+//  Created by Lukas Thoms on 7/12/15.
+//  Copyright (c) 2015 Lukas Thoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTLScheduleTableViewCell : UITableViewCell
+
+@end
---
+++ WhereTheL/WTLScheduleTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  WTLScheduleTableViewCell.m
+//  WhereTheL
+//
+//  Created by Lukas Thoms on 7/12/15.
+//  Copyright (c) 2015 Lukas Thoms. All rights reserved.
+//
+
+#import "WTLScheduleTableViewCell.h"
+
+@implementation WTLScheduleTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
