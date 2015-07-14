1fff0e6bff0b325877b4832095386a38e718874d
lukasthoms <luk@stho.ms>
2015-07-14 09:25:45 -0400

    working(ish) before Parse integration

---
+++ WhereTheL/WTLScheduleTableView.h
@@ -0,0 +1,13 @@
+//
+//  WTLScheduleTableView.h
+//  WhereTheL
+//
+//  Created by Lukas Thoms on 7/13/15.
+//  Copyright (c) 2015 Lukas Thoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTLScheduleTableView : UITableView
+
+@end
---
+++ WhereTheL/WTLScheduleTableView.m
@@ -0,0 +1,21 @@
+//
+//  WTLScheduleTableView.m
+//  WhereTheL
+//
+//  Created by Lukas Thoms on 7/13/15.
+//  Copyright (c) 2015 Lukas Thoms. All rights reserved.
+//
+
+#import "WTLScheduleTableView.h"
+
+@implementation WTLScheduleTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
